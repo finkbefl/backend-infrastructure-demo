77b5933a9da9495600c3a32c4e387edc1e654113
Florian Finkbeiner <finkbeinerflorian@yahoo.de>
2022-11-05 12:17:25 +0100

    Added first version of data_processing microservice with minor enhancements of data_collection

--- collection/data_collection/main.py
+++ collection/data_collection/main.py
@@ -7,8 +7,13 @@ from prometheus_client import Counter, start_http_server
 
 SERVICE_NAME = "data_collection"
 
-# Event counter for monitoring the received sensor values with prometheus
-GET_SENSOR_DATA_COUNT = Counter("sensor_data_received", "The number of sensor-data values received trom sensor-data1 topic")
+# Event counter for monitoring with prometheus
+# The received sensor values
+SENSOR_VALUES_COUNT = Counter("sensor_values_received", "The number of sensor-data values received trom sensor-data1 topic")
+# The collected sensor values
+COLLECTED_VALUES_COUNT = Counter("sensor_values_collected", "The number of sensor-data values collected")
+# The sent sensor values
+SENT_VALUES_COUNT = Counter("sensor_values_sent", "The number of sensor-data values sent to src-data topic")
 
 # Configure the logging mechanism
 logging.basicConfig(
@@ -21,19 +26,27 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 app = faust.App(SERVICE_NAME, broker="broker:29092", value_serializer='raw')
-sensor_data_topic = app.topic("sensor-data1", partitions=8)
+sensor_data_topic_1 = app.topic("sensor-data1", partitions=8)
+src_data_topic = app.topic("src-data", partitions=8)
 
 
-@app.agent(sensor_data_topic)
+@app.agent(sensor_data_topic_1)
 async def on_event(stream) -> None:
     async for msg_key, msg_value in stream.items():
-        # At first imcrement the event counter for prometheus monitoring
-        GET_SENSOR_DATA_COUNT.inc()
-        logger.info(f'Received new pair message {msg_value}')
+        # At first imcrement the event counter for prometheus monitoring of the received sensor values
+        SENSOR_VALUES_COUNT.inc()
+        logger.info(f'Received new sensor value {msg_value}')
         serialized_message = json.loads(msg_value)
-        for pair_name, pair_value in serialized_message.items():
-            logger.info(f"Key: {msg_key} - Extracted pair: {pair_name}: {pair_value}")
-            yield msg_value
+        #for pair_name, pair_value in serialized_message.items():
+        #    logger.info(f"Key: {msg_key} - Extracted pair: {pair_name}: {pair_value}")
+        # Increment the prometheus counter for collected values
+        COLLECTED_VALUES_COUNT.inc()
+        logger.info(f'Publish the sensor value to {src_data_topic}')
+        # Send the sensor value with the numer of the sensor extracted from the received topic sensor_data_topic_*
+        await src_data_topic.send(timestamp=float(serialized_message['timestamp']), key="sensor_value", value=json.dumps({'sensor_num':''.join(filter(lambda i: i.isdigit(), str(msg_key))), 'value':serialized_message['value']}).encode())
+        # Increment the prometheus counter for sent values
+        SENT_VALUES_COUNT.inc()
+        
 
 @app.task
 async def on_started() -> None:
---
+++ processing/data_processing/main.py
@@ -0,0 +1,72 @@
+# Processing the src-data from kafka
+
+import logging
+import json
+import faust
+from prometheus_client import Counter, start_http_server
+
+SERVICE_NAME = "data_processing"
+
+# Event counter for monitoring with prometheus
+# The received sensor values
+SENSOR_VALUES_COUNT = Counter("sensor_values_received", "The number of sensor-data values received trom src_data topic")
+# The processed sensor values
+PROCESSED_VALUES_COUNT = Counter("sensor_values_processed", "The number of sensor-data values processed")
+# The sent sensor values
+SENT_VALUES_COUNT = Counter("sensor_values_sent", "The number of sensor-data values sent to processed-data topic")
+
+# Configure the logging mechanism
+logging.basicConfig(
+    # Default Level
+    level = logging.INFO,
+    # Formatting the logs
+    format="%(asctime)s,%(msecs)d%(levelname)-8s[%(name)s:%(filename)s(%(lineno)d)]%(message)s")
+
+# Get the logger
+logger = logging.getLogger(__name__)
+
+app = faust.App(SERVICE_NAME, broker="broker:29092", value_serializer='raw')
+src_data_topic = app.topic("src-data", partitions=8)
+processed_data_topic = app.topic("processed-data", partitions=8)
+
+def process_data_value(sensor_value):
+    # TODO: Check for missing values, outliers, exceptions,...
+    # All sensor values are processed as integer, but maybe we will receive float numbers
+    if is_float(sensor_value):
+        logger.info(f'Sensor value of type float received, convert to int')
+        return float(sensor_value)
+    else:
+        logger.info(f'Sensor value of type int received, no conversion')
+        return int(sensor_value)
+
+def is_float(value):
+  try:
+    float(value)
+    return True
+  except:
+    return False
+
+@app.agent(src_data_topic)
+async def on_event(stream) -> None:
+    async for msg_key, msg_value in stream.items():
+        # At first imcrement the event counter for prometheus monitoring of the received sensor values
+        SENSOR_VALUES_COUNT.inc()
+        logger.info(f'Key: {msg_key} - Received new sensor value {msg_value}')
+        serialized_message = json.loads(msg_value)
+        #for pair_name, pair_value in serialized_message.items():
+        #    logger.info(f"Key: {msg_key} - Extracted pair: {pair_name}: {pair_value}")
+        # Process the data (cast float sensor values to int)
+        serialized_message['value'] = process_data_value(serialized_message['value'])
+        # Increment the prometheus counter for processed values
+        PROCESSED_VALUES_COUNT.inc()
+        logger.info(f'Publish the sensor value to {processed_data_topic}')
+        # Send the sensor value with the numer of the sensor extracted from the received topic sensor_data_topic_*
+        await processed_data_topic.send(timestamp=stream.current_event.message.timestamp, key="sensor_value", value=json.dumps({'sensor_num':''.join(filter(lambda i: i.isdigit(), str(msg_key))), 'value':serialized_message['value']}).encode())
+        # Increment the prometheus counter for sent values
+        SENT_VALUES_COUNT.inc()
+        
+
+@app.task
+async def on_started() -> None:
+    logger.info('Starting prometheus server to expose the metrics on port=7003')
+    start_http_server(port=7003)
