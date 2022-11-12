5c950d9dc0fcfe280de1117670cd2b95c040516e
Florian Finkbeiner <finkbeinerflorian@yahoo.de>
2022-11-13 00:07:04 +0100

    Added calculation of the age of requested latest values and added in prometheus and grafana

--- api-gateway/api_gateway/main.py
+++ api-gateway/api_gateway/main.py
@@ -2,9 +2,12 @@
 
 import logging
 from typing import List, Dict
+import datetime
 from fastapi import FastAPI
 from database import Database
 from prometheus_client import Counter, Histogram, start_http_server
+
+_INF = float("inf")
 
 # Event counter for monitoring with prometheus
 # Number of sensor values requests
@@ -13,6 +16,8 @@ SENSOR_VALUES_COUNT = Counter("sensor_values_requests", "The number of sensor-da
 AGGREGATED_VALUES_COUNT = Counter("aggregated_values_requests", "The number of aggregated values requests via REST API")
 # Number of latest sensor value requests
 LATEST_VALUE_COUNT = Counter("latest_value_requests", "The number of latest value requests via REST API")
+# Age of latest sensor value request
+LATEST_VALUE_AGE = Histogram('latest_value_requests_age_seconds', 'Cumulative histogram of age in seconds of latest value requests', buckets=(0.2, 0.4, 0.6, 0.8, 1, 1.2, _INF))
 
 
 # Configure the logging mechanism
@@ -44,11 +49,17 @@ async def get_average_values(sensor_num: int, timestamp_start: float, timestamp_
     AGGREGATED_VALUES_COUNT.inc()
     return await db.get_average_values(sensor_num, timestamp_start, timestamp_end)
 
-@fastapi_app.get("/temperature_realtime")
-async def get_temperature_realtime(sensor_num: int) -> int:
-    logger.info("Get latest realtime temp")
+@fastapi_app.get("/temperature_latest")
+async def get_temperature_latest(sensor_num: int) -> int:
+    logger.info("Get latest temp")
     LATEST_VALUE_COUNT.inc()
-    return await db.get_latest_temperature_value(sensor_num)
+    # Calc age in seconds
+    received_data = await db.get_latest_temperature_value(sensor_num)
+    # TODO Error handling (e.g. no data received)
+    age = datetime.datetime.now().timestamp() - float(received_data[0]['timestamp'])
+    logger.info(f"Calculated age of latest temp: {age}")
+    LATEST_VALUE_AGE.observe(age)
+    return received_data
 
 @fastapi_app.on_event("startup")
 async def startup_event():
