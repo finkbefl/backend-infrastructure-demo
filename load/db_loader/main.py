0de854502005162051afa89d993ded010b6d7b02
Florian Finkbeiner <finkbeinerflorian@yahoo.de>
2022-11-13 01:08:57 +0100

    Added latency of sensor value processing and aggregation to grafana dashboard calculated within db_loader

--- load/db_loader/main.py
+++ load/db_loader/main.py
@@ -1,18 +1,25 @@
 # Load processed and aggregated data to the database
 
 import logging
+import datetime
 import json
 import faust
 from database import Database
-from prometheus_client import Counter, start_http_server
+from prometheus_client import Counter, Histogram, start_http_server
 
 SERVICE_NAME = "db_loader"
+
+_INF = float("inf")
 
 # Event counter for monitoring with prometheus
 # The received sensor values
 SENSOR_VALUES_COUNT = Counter("sensor_values_loaded", "The number of sensor-data values received from processed-data topic and loaded into db")
 # The aggregated sensor values
 AGGREGATED_VALUES_COUNT = Counter("aggregated_values_loaded", "The number of aggregated sensor-data values received from topic aggregated-data and loaded into db")
+# Latency of received sensor values
+SENSOR_VALUE_LATENCY = Histogram('sensor_value_latency', 'Cumulative histogram of latency in seconds of sensor values', buckets=(0.01, 0.02, 0.03, 0.04, 0.05, 0.1, _INF))
+# Latency of received aggregated values
+AGGREGATED_VALUE_LATENCY = Histogram('aggregated_value_latency', 'Cumulative histogram of latency in seconds of aggregated values', buckets=(0.01, 0.02, 0.03, 0.04, 0.05, 0.1, _INF))
 
 # Configure the logging mechanism
 logging.basicConfig(
@@ -40,6 +47,10 @@ async def on_event(stream) -> None:
         SENSOR_VALUES_COUNT.inc()
         # Load the temperature into the database
         await db.save_temperature(timestamp=float(stream.current_event.message.timestamp), sensor_num=int(serialized_message['sensor_num']), value=int(serialized_message['value']))
+        # Calculate Latency and provide it to prometheus
+        latency = datetime.datetime.now().timestamp() - float(stream.current_event.message.timestamp)
+        logger.info(f"Calculated latency: {latency}")
+        SENSOR_VALUE_LATENCY.observe(latency)
 
 @app.agent(average_changelog_topic)
 async def on_average_event(stream) -> None:
@@ -50,6 +61,10 @@ async def on_average_event(stream) -> None:
         AGGREGATED_VALUES_COUNT.inc()
         # Load the average into the database
         await db.save_average(timestamp=float(stream.current_event.message.timestamp), sensor_num=int(json.loads(msg_key)), value=float(serialized_message['average']))
+        # Calculate Latency and provide it to prometheus
+        latency = datetime.datetime.now().timestamp() - float(stream.current_event.message.timestamp)
+        logger.info(f"Calculated latency: {latency}")
+        AGGREGATED_VALUE_LATENCY.observe(latency)
         
 
 @app.task
